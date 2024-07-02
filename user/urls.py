27e481e26bf623bfe2f926939de7cde6db4dc12c
adel <adel.khedri.mirqaid@gmail.com>
2024-07-01 23:16:35 -0700

    add [forget password, logout] views

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('sinup/', views.SinupView.as_view(), name="sinup"),
     path('sinup/active/<str:email>/<str:code>', views.ActiveAccountView.as_view(), name="active account"),
+    path('forgetPassword/', views.ForegetPasswordView.as_view(), name="forget password"),
+    path('forgetPassword/confirm/<str:email>/<str:code>', views.ConfirmForgetPasswordView.as_view(), name="confirm forget password"),
+    path('logout', views.logout_view, name='logout'),
 ]
 
 if settings.DEBUG:
